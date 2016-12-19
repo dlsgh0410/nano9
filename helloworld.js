2866252319f0487b6f968d90dd91a0a49a774eaf
dlsgh0410 <dlsgh0410@hanmail.net>
2016-12-19 14:49:28 +0900

    Modify github

--- helloworld.js
+++ helloworld.js
@@ -4,4 +4,4 @@ console.log('Tell your World');
 console.log('Tell his world');
 console.log('Tell her world');
 console.log('Tell our world');
-colsole.log('Modify github remote repository');
+console.log('Modify github remote repository');
